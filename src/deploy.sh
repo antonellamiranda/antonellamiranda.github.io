550c1b94649d72ba343c658d3205b205ea1d8dff
G Faller <german.faller@xpo.com>
2023-02-24 15:31:31 -0300

    Baby Step

---
+++ src/deploy.sh
@@ -0,0 +1,12 @@
+npm run build
+npm run generate
+
+cd ..
+
+rm -rf docs
+mkdir docs
+echo www.antonellamiranda.com > docs/CNAME
+cp -rT src/dist docs/
+git add .
+git commit -m "Deploy  -  "$1
+git push
