8000a005517e2004c704fcfd6f15823e66e519dd
Germán Faller <gfaller@pax8.com>
2023-02-24 16:51:00 -0300

    clear

--- src/deploy.sh
+++ src/deploy.sh
@@ -5,7 +5,7 @@ cd ..
 
 rm -rf docs
 mkdir docs
-echo www.antonellamiranda.com > docs/CNAME
+# echo www.antonellamiranda.com > docs/CNAME
 cp -rT src/dist docs/
 git add .
 git commit -m "Deploy  -  "$1
