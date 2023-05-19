61afae84ca04a6f9fa91e2360f4919bccfdcac1d
Falucho <faller222@gmail.com>
2023-05-19 13:36:45 -0300

    Update

--- src/components/First.vue
+++ src/components/First.vue
@@ -1,29 +1,60 @@
 <template>
-    <div id="first">
-        <div class="container">
-            <img id="antonella_miranda" alt="antonella_miranda" src="@/assets/img/antonella_miranda_4.jpg">
-            <img src="@/assets/img/corner1/f1.svg">
-            <img src="@/assets/img/corner1/f2.svg">
-        </div>
+  <div id="first">
+    <div class="container">
+      <img
+        id="antonella_miranda"
+        alt="antonella_miranda"
+        src="@/assets/img/antonella_miranda_4.jpg"
+      />
+      <img id="corner" alt="corner" src="@/assets/img/corner1/f.svg" />
+      <!--      <span id="name1">Antonella</span>-->
+      <!--      <span id="name2">Miranda</span>-->
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'FirstComponent',
+  name: 'FirstComponent',
 }
 </script>
 
 <style lang="scss">
-
 #first {
   background-color: blue; //Debug
   .container {
+    position: relative;
     background-color: aqua; //Debug
     max-width: 1366px;
     margin-right: auto;
     margin-left: auto;
+  }
 
+  img {
+    //position: absolute;
+  }
+
+  #name1 {
+    font-size: 5.5rem;
+    position: absolute;
+    left: 1rem;
+  }
+
+  #name2 {
+    font-size: 4.5rem;
+    position: absolute;
+    left: 1rem;
+    top: 6rem;
+  }
+
+  #corner {
+    width: 85%;
+    position: absolute;
+    left: 0;
+  }
+
+  #antonella_miranda {
+    width: 100%;
   }
 
   //position: relative;
@@ -42,6 +73,5 @@ export default {
   //  margin-right: auto;
   //  width: 100%;
   //}
-
 }
 </style>
--- src/components/MainView.vue
+++ src/components/MainView.vue
@@ -1,13 +1,13 @@
 <template>
-    <div id="main-view">
-        <first/>
-        <second/>
-    </div>
+  <div id="main-view">
+    <first />
+    <!--        <second/>-->
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'EmptyComponent',
+  name: 'EmptyComponent',
 }
 </script>
 
